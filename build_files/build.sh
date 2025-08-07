16d6fcf1da936ee1083709e39708f96331d83a38
Kadin Buckton <setenforce@protonmail.com>
2025-08-07 15:05:09 -0400

    Add Niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,24 @@
 
 set -ouex pipefail
 
+### Install Repos
+
+dnf5 -y copr enable yalter/niri
+dnf5 -y copr enable ulysg/xwayland-satellite
+
+### Dependency Groups
+
+NIRI_DEPS=(
+    fuzzel
+    mako
+    xwayland-satellite
+    swaylock
+)
+
+ADDITIONAL_PACKAGES=(
+    alacritty
+)
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +28,15 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+    tmux \
+    ${NIRI_DEPS[@]} \
+    ${ADDITIONAL_PACKAGES[@]}
+
+### Disable Repositories
+
+dnf5 -y copr disable yalter/niri
+dnf5 -y copr disable ulysg/xwayland-satellite
 
 # Use a COPR Example:
 #
